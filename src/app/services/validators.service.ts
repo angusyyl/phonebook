53c1223417a2444d2549a515ec5193b3c20d688e
Angus <angusyylhk@gmail.com>
2022-06-10 10:29:47 +0800

    fix: wrong validation on name fields

--- src/app/services/validators.service.ts
+++ src/app/services/validators.service.ts
@@ -10,7 +10,7 @@ export class ValidatorsService extends Validators {
     const firstName = fg.get('firstName');
     const middleName = fg.get('middleName');
     const lastName = fg.get('lastName');
-    if ((firstName?.value == null && middleName?.value == null && lastName?.value == null) || (firstName?.value == '' && middleName?.value == '' && lastName?.value == '')) {
+    if ((firstName?.value == null || firstName?.value == '') && (middleName?.value == null || middleName?.value == '') && (lastName?.value == null || lastName?.value == '')) {
       return { nameIsEmpty: true };
     }
     return null;
