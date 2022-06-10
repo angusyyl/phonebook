e791a1be800f11a9475535eede0fe4a610ae27df
Angus <angusyylhk@gmail.com>
2022-06-10 16:17:50 +0800

    fix: deployment configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAsKWiJvFGJqUTPcIZ-vfqxvxB_pQ9juTU",
+    authDomain: "phonebook-a7ad4.firebaseapp.com",
+    databaseURL: "https://phonebook-a7ad4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "phonebook-a7ad4",
+    storageBucket: "phonebook-a7ad4.appspot.com",
+    messagingSenderId: "741712332308",
+    appId: "1:741712332308:web:922e6a857f09b6d5cb0901"
+  }
 };
